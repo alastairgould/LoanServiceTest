be1c00b51ef9599b64ce236b1bbf8da876f1196e
Alastair Gould <me@alastairgould.co.uk>
2026-05-19 15:17:48 +0100

    Refactor apply for loan controller.

--- LoanApplication/Features/ApplyForLoan/ApplyForLoanController.cs
+++ LoanApplication/Features/ApplyForLoan/ApplyForLoanController.cs
@@ -24,25 +24,23 @@ public class ApplyForLoanController : ControllerBase
     public IActionResult Apply([FromBody] LoanApplicationRequest request)
     {
         var errors = _validator.Validate(request);
-        
+
         if (errors.Count > 0)
             return ValidationProblem(new ValidationProblemDetails(errors));
 
-        var id = Guid.NewGuid();
-        var createdAt = _timeProvider.GetUtcNow().UtcDateTime;
-
-        _loanContext.LoanApplications.Add(new LoanApplication(
-            id,
+        var loan = new LoanApplication(
+            Guid.NewGuid(),
             request.Name,
             request.Email,
             (int)request.MonthlyIncome,
             request.Amount,
             request.TermMonths,
             "Pending",
-            createdAt,
-            null));
+            _timeProvider.GetUtcNow().UtcDateTime,
+            null);
 
+        _loanContext.LoanApplications.Add(loan);
         _loanContext.SaveChanges();
-        return Ok(new LoanApplicationResult(id, LoanStatus.Pending, createdAt));
+        return Ok(new LoanApplicationResult(loan.Id, LoanStatus.Pending, loan.CreatedAt));
     }
 }
